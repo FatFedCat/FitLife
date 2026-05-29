7a54f20798ac3ade0a6acca3a19329f8182f57d8
FatFedCat <baev.fedar@yandex.ru>
2026-05-29 13:13:00 +0500

    Исправил пробел у 2 print, теперь его задаёт агрумент end в первом print, разделил структуру на блоки, добавил константу для воды, добавил блок проверок кода, на входные данные

--- fit_life.py
+++ fit_life.py
@@ -1,21 +1,50 @@
 # Проект FitLife - MVP версия 1.0
 
+#блок проверки ввода данных
+def checking_age_for_integer(user_input):
+    while True:
+        try:
+            return int(user_input)
+        except ValueError:
+            print("Пожалуйста, введите целое число.")
+            user_input = input("Введите ваш возраст(целое число): ")
 
+def checking_weight_for_float(user_input):
+    while True:
+        try:
+            return float(user_input)
+        except ValueError:
+            print("Пожалуйста, введите число.")
+            user_input = input("Введите ваш вес(в кг): ")
+
+def checking_height_for_float(user_input):
+    while True:
+        try:
+            return float(user_input)
+        except ValueError:
+            print("Пожалуйста, введите число.")
+            user_input = input("Введите ваш рост(в метрах): ")
+
+#блок констант
+# Норма воды 30мл на кг веса
+WATER_NORMAL = 0.03
+
+#блок ввода данных
 # 1. Знакомство
 user_name = str(input("Введите ваше имя: "))
-user_age = int(input("Введите ваш возраст(целое число): "))
+user_age = checking_age_for_integer(input("Введите ваш возраст(целое число): "))
 
 # 2. Сбор данных
-user_weight = float(input("Введите ваш вес(в кг): "))
-user_height = float(input("Введите ваш рост(в метрах): "))
+user_weight = checking_weight_for_float(input("Введите ваш вес(в кг): "))
+user_height = checking_height_for_float(input("Введите ваш рост(в метрах): "))
 
 # 3. Логика расчетов
 bmi = round(user_weight / (user_height ** 2), 1)
 
 # Подсчет воды: вес * 30 мл
-water_needed = round(user_weight * 0.03, 2)
+water_needed = round(user_weight * WATER_NORMAL, 2)
 
 # 4. Вывод красивого результата
-print(f"Привет, {user_name}! Твой возраст: {user_age} лет,", end="")
-print(f" ИМТ: {bmi}, Норма воды: {water_needed} л.")
+print(f"Привет, {user_name}! Твой возраст: {user_age} лет,", end=" ")
+print(f"ИМТ: {bmi}, Норма воды: {water_needed} л.")
 print("Расчет окончен. Будьте здоровы!")
